ae614466788bbd74b492b1f536ae1b7a4d794de9
Micha Riser <micha@povworld.org>
2016-08-12 01:09:48 +0200

    Use a local static instance of Objenesis.

--- com.simpleentity.serialize/src/com/simpleentity/serialize/object/GenericObjectSerializer.java
+++ com.simpleentity.serialize/src/com/simpleentity/serialize/object/GenericObjectSerializer.java
@@ -4,61 +4,67 @@
  */
 package com.simpleentity.serialize.object;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
 import org.objenesis.instantiator.ObjectInstantiator;
 
-import com.simpleentity.serialize.context.*;
-import com.simpleentity.util.*;
+import com.simpleentity.serialize.context.IObjectDeserializationContext;
+import com.simpleentity.serialize.context.IObjectSerializationContext;
+import com.simpleentity.util.Assert;
 
 public class GenericObjectSerializer<T> implements IObjectSerializer<T> {
-  
-  private final List<ISerializationEntry> fSerializationEntries = new ArrayList<ISerializationEntry>();
-  private final ObjectInstantiator        fInstantiator;
 
-  public GenericObjectSerializer(Class<?> clazz) {
-    fInstantiator = ObjectUtil.INSTANCE.getInstantiatorOf(clazz);
-  }
+	private static final Objenesis OBJENESIS = new ObjenesisStd();
 
-  public void add(ISerializationEntry entry) {
-    fSerializationEntries.add(entry);
-  }
+	private final List<ISerializationEntry> fSerializationEntries = new ArrayList<ISerializationEntry>();
+	private final ObjectInstantiator fInstantiator;
 
-  @Override
-  public void serialize(IObjectSerializationContext context, T object) {
-    try {
-      doSerialize(context, object);
-    } catch (IllegalArgumentException e) {
-      Assert.fail(e);
-    } catch (IllegalAccessException e) {
-      Assert.fail(e);
-    }
-  }
+	public GenericObjectSerializer(Class<?> clazz) {
+		fInstantiator = OBJENESIS.getInstantiatorOf(clazz);
+	}
 
-  private void doSerialize(IObjectSerializationContext context, T object) throws IllegalAccessException {
-    for (ISerializationEntry entry: fSerializationEntries) {
-      entry.write(context, object);
-    }
-  }
+	public void add(ISerializationEntry entry) {
+		fSerializationEntries.add(entry);
+	}
 
-  @Override
-  public T deserialize(IObjectDeserializationContext context) {
-    try {
-      return doDeserialize(context);
-    } catch (IllegalArgumentException e) {
-      throw Assert.fail(e);
-    } catch (IllegalAccessException e) {
-      throw Assert.fail(e);
-    }
-  }
+	@Override
+	public void serialize(IObjectSerializationContext context, T object) {
+		try {
+			doSerialize(context, object);
+		} catch (IllegalArgumentException e) {
+			Assert.fail(e);
+		} catch (IllegalAccessException e) {
+			Assert.fail(e);
+		}
+	}
 
-  @SuppressWarnings("unchecked")
-  private T doDeserialize(IObjectDeserializationContext context) throws IllegalAccessException {
-    T result = (T) fInstantiator.newInstance();
-    for (ISerializationEntry entry: fSerializationEntries) {
-      entry.read(context, result);
-    }
-    return result;
-  }
+	private void doSerialize(IObjectSerializationContext context, T object) throws IllegalAccessException {
+		for (ISerializationEntry entry : fSerializationEntries) {
+			entry.write(context, object);
+		}
+	}
+
+	@Override
+	public T deserialize(IObjectDeserializationContext context) {
+		try {
+			return doDeserialize(context);
+		} catch (IllegalArgumentException e) {
+			throw Assert.fail(e);
+		} catch (IllegalAccessException e) {
+			throw Assert.fail(e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private T doDeserialize(IObjectDeserializationContext context) throws IllegalAccessException {
+		T result = (T) fInstantiator.newInstance();
+		for (ISerializationEntry entry : fSerializationEntries) {
+			entry.read(context, result);
+		}
+		return result;
+	}
 
 }
