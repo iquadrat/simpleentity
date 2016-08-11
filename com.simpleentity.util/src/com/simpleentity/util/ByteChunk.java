52e961d53c83f882d13585f295296f12860ea4ab
Micha Riser <micha@povworld.org>
2016-08-12 01:11:07 +0200

    Add ByteChunk class.

---
+++ com.simpleentity.util/src/com/simpleentity/util/ByteChunk.java
@@ -0,0 +1,143 @@
+package com.simpleentity.util;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.povworld.collection.common.ObjectUtil;
+
+public class ByteChunk {
+
+	public static final int DEFAULT_INITIAL_BUILDER_CAPACITY = 20;
+
+	private final byte[] bytes;
+	private final int length;
+
+	private ByteChunk(byte[] bytes, int length) {
+		this.bytes = bytes;
+		this.length = length;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public byte get(int index) {
+		if (index >= length) {
+			throw new IndexOutOfBoundsException(index + ">=" + length);
+		}
+		return bytes[index];
+	}
+
+	public void get(ByteBuffer buffer) {
+		buffer.put(bytes, 0, length);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 1;
+		for (int i = 0; i < length; ++i) {
+			result = 31 * result + bytes[i];
+		}
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		ByteChunk other = ObjectUtil.castOrNull(object, ByteChunk.class);
+		if ((other == null) || (other.length != this.length)) {
+			return false;
+		}
+		for (int i = 0; i < length; ++i) {
+			if (other.bytes[i] != this.bytes[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return StringUtil.bytesToHex(bytes, 0, length);
+	}
+
+	public static Builder newBuilder() {
+		return newBuilder(DEFAULT_INITIAL_BUILDER_CAPACITY);
+	}
+
+	public static Builder newBuilder(int initialCapacity) {
+		return new Builder(initialCapacity);
+	}
+
+	public static class Builder {
+		private byte[] bytes;
+		private int size = 0;
+		private ByteOrder byteOrder = ByteOrder.LITTLE_ENDIAN;
+
+		protected Builder(int initialCapacity) {
+			this.bytes = new byte[initialCapacity];
+		}
+
+		public Builder setByteOrder(ByteOrder byteOrder) {
+			this.byteOrder = byteOrder;
+			return this;
+		}
+
+		public Builder append(byte[] bytes) {
+			ensureCapacity(size + bytes.length);
+			System.arraycopy(bytes, 0, this.bytes, size, bytes.length);
+			size += bytes.length;
+			return this;
+		}
+
+		public Builder append(byte[] bytes, int offset, int length) {
+			ensureCapacity(size + length);
+			System.arraycopy(bytes, offset, this.bytes, size, length);
+			size += length;
+			return this;
+		}
+
+		public Builder append(ByteBuffer buffer) {
+			int length = buffer.remaining();
+			ensureCapacity(size + length);
+			buffer.get(this.bytes, size, length);
+			return this;
+		}
+
+		public Builder append(ByteChunk chunk) {
+			append(chunk.bytes, 0, chunk.length);
+			return this;
+		}
+
+		public Builder appendInt(int n) {
+			ensureCapacity(size + 4);
+			ByteBuffer buffer = ByteBuffer.wrap(bytes, size, 4);
+			buffer.order(byteOrder);
+			buffer.putInt(n);
+			return this;
+		}
+
+		public Builder appendLong(long n) {
+			ensureCapacity(size + 8);
+			ByteBuffer buffer = ByteBuffer.wrap(bytes, size, 8);
+			buffer.order(byteOrder);
+			buffer.putLong(n);
+			return this;
+		}
+
+		public ByteChunk builder() {
+			// TODO return constant if size is 0
+			return new ByteChunk(bytes, size);
+		}
+
+		private void ensureCapacity(int capacity) {
+			if (size < capacity) {
+				int newCapacity = Math.max(capacity, size + size / 2);
+				byte[] newBytes = new byte[newCapacity];
+				System.arraycopy(bytes, 0, newBytes, 0, size);
+				bytes = newBytes;
+			}
+		}
+
+	}
+
+}
--- com.simpleentity.util/src/com/simpleentity/util/StringUtil.java
+++ com.simpleentity.util/src/com/simpleentity/util/StringUtil.java
@@ -1,22 +1,47 @@
 package com.simpleentity.util;
 
+
 public class StringUtil {
 
-  private StringUtil() {}
+	private StringUtil() {
+	}
 
-  /**
-   * Splits a file name in name and extension.
-   * @param name the file name to split
-   * @return an array of two elements: The file name and the extension.
-   */
-  public static String[] getFileNameAndExtension(String name) {
-    int lastDot = name.lastIndexOf('.');
-    if (lastDot == -1) {
-      return new String[] {name, ""};
-    }
-    String prefix = name.substring(0, lastDot);
-    String postfix = name.substring(lastDot + 1);
-    return new String[] {prefix, postfix};
-  }
+	/**
+	 * Splits a file name in name and extension.
+	 * 
+	 * @param name
+	 *            the file name to split
+	 * @return an array of two elements: The file name and the extension.
+	 */
+	public static String[] getFileNameAndExtension(String name) {
+		int lastDot = name.lastIndexOf('.');
+		if (lastDot == -1) {
+			return new String[] { name, "" };
+		}
+		String prefix = name.substring(0, lastDot);
+		String postfix = name.substring(lastDot + 1);
+		return new String[] { prefix, postfix };
+	}
+
+	public static String bytesToHex(byte[] bytes) {
+		return bytesToHex(bytes, 0, bytes.length);
+	}
+
+	public static String bytesToHex(byte[] bytes, int offset, int length) {
+		StringBuilder result = new StringBuilder(length * 2);
+		for (int i = offset; i < offset + length; ++i) {
+			byte b = bytes[i];
+			result.append(halfByteToChar((b & 0xff) >>> 4));
+			result.append(halfByteToChar((b & 15)));
+		}
+		return result.toString();
+	}
+	
+	private static char halfByteToChar(int halfByte) {
+		if (halfByte < 10) {
+			return (char) ('0' + halfByte);
+		}
+		return (char) ('a' + (halfByte - 10));
+	}
 
 }
