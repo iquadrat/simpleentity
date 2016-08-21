91a019b435e92f3ef3f2e48ee166a04f4fb5b00c
Micha Riser <micha@povworld.org>
2016-08-21 14:21:59 +0200

    Code cleanup.

--- com.simpleentity.util/src/com/simpleentity/util/logger/Logger.java
+++ com.simpleentity.util/src/com/simpleentity/util/logger/Logger.java
@@ -1,17 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2010 actifsource GmbH.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     actifsource GmbH - initial API and implementation
- ******************************************************************************/
-/*
- * Created on 20.09.2005
- *
- */
 package com.simpleentity.util.logger;
 
 import com.simpleentity.util.logger.LogHandler.LogLevel;
